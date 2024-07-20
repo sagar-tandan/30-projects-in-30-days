bd7f591c6ec78cdf3b94612fe9e1cde987be3e1d
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-20 13:42:30 +0545

    done upto here

--- Day 9 - Drink Water/scripts.js
+++ Day 9 - Drink Water/scripts.js
@@ -1,11 +1,14 @@
 const button = document.getElementById("btn");
-const container = document.querySelector(".container");
+const toasts = document.querySelector(".toasts");
+const input = document.querySelector(".message");
 
 button.addEventListener("click", () => {
+  const message = input.value;
+  console.log(message);
   const toast = document.createElement("div");
   toast.classList.add("toast");
-  toast.innerText = "Hello";
-  container.appendChild(toast);
+  toast.innerText = message ? message : "Random Message";
+  toasts.appendChild(toast);
 
   // Adding the active class after a short delay
   setTimeout(() => {
@@ -18,6 +21,4 @@ button.addEventListener("click", () => {
       toast.remove();
     }, 100);
   }, 3000);
-
-  
 });
