dad0a3789ca3994e920f66d7c04e6e9696398b24
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-20 13:17:57 +0545

    toast showing work done

--- Day 9 - Drink Water/scripts.js
+++ Day 9 - Drink Water/scripts.js
@@ -1,5 +1,23 @@
-const button = document.getElementById('btn');
-button.addEventListener('click',()=>{
-    const toast = document.querySelector('.toast')
-    toast.classList.add('active')
-})
+const button = document.getElementById("btn");
+const container = document.querySelector(".container");
+
+button.addEventListener("click", () => {
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerText = "Hello";
+  container.appendChild(toast);
+
+  // Adding the active class after a short delay
+  setTimeout(() => {
+    toast.classList.add("active");
+  }, 10);
+
+  setTimeout(() => {
+    toast.classList.remove("active");
+    setTimeout(() => {
+      toast.remove();
+    }, 100);
+  }, 3000);
+
+  
+});
