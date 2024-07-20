eeb8a9c18ef98dd0250ac669edcdab79d2599a8c
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-20 16:50:25 +0545

    Image Slider Completed

--- Day 10 - Image Carousel/scripts.js
+++ Day 10 - Image Carousel/scripts.js
@@ -1,13 +1,19 @@
 const carousel = document.querySelector(".carousel");
 const image_container = document.querySelector(".images");
 const allImages = document.querySelectorAll("img");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
 let index = 0;
 
-setInterval(slide, 2000);
+let interval = setInterval(slide, 2000);
 
 function slide() {
   index++;
+  ChangeImages();
+}
+
+function ChangeImages() {
   if (index > allImages.length - 1) {
     index = 0;
   } else if (index < 0) {
@@ -15,3 +21,20 @@ function slide() {
   }
   image_container.style.transform = `translate(-${index * 1024}px)`;
 }
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(slide, 2000);
+}
+
+prev.addEventListener("click", () => {
+  index--;
+  ChangeImages();
+  resetInterval();
+});
+
+next.addEventListener("click", () => {
+  index++;
+  ChangeImages();
+  resetInterval();
+});
