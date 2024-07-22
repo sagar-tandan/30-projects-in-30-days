e949ec0cb40e9d64cb2800b7a6cee3cb3ec163c2
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-22 14:07:53 +0545

    js started

---
+++ Day 9-11- Quiz App/scripts.js
@@ -0,0 +1,27 @@
+const quizData = [
+  {
+    id: 1,
+    question: "Which lamguage runs in web browser ?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    id: 1,
+    question: "Which lamguage runs in web browser ?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+];
+
+const question = document.getElementById("question");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option4 = document.getElementById("option4");
+const button = document.getElementById("btn");
