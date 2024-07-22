4c073c4e959ea33c0f62207ff3b0f5c24d766bb0
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-22 15:53:19 +0545

    js started for hoverboard but a bit bugs

--- Day 9-12- Hoverboard/scripts.js
+++ Day 9-12- Hoverboard/scripts.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 
+const color = ["#fff", "#f2f2f2", "#c2c2c2"];
 let SQUARE = 500;
 
 for (let i = 0; i < SQUARE; i++) {
@@ -8,3 +9,17 @@ for (let i = 0; i < SQUARE; i++) {
   container.appendChild(element);
 }
 
+const allSquare = document.querySelectorAll(".square");
+
+allSquare.forEach((square) => {
+  square.addEventListener("mouseover", setColor(square));
+});
+
+function setColor(element) {
+  const color = getRandomColor();
+  console.log(color);
+}
+function getRandomColor() {
+  const randomColor = Math.floor(Math.random() * color.length);
+  return randomColor;
+}
