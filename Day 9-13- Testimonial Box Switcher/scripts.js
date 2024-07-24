5118d9e47a6a676c7d4e0cfbd1fb9e5730a0b461
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-24 15:49:06 +0545

    js started

---
+++ Day 9-13- Testimonial Box Switcher/scripts.js
@@ -0,0 +1,66 @@
+const testimonial = document.querySelector(".comment");
+const image = document.getElementById("image");
+const name = document.getElementById("name");
+const profession = document.getElementById("profession");
+const allComments = [
+  {
+    id: "1",
+    image: "https://example.com/images/will-smith.jpg",
+    name: "Will Smith",
+    profession: "Actor",
+  },
+  {
+    id: "2",
+    image: "https://example.com/images/emma-watson.jpg",
+    name: "Emma Watson",
+    profession: "Actress",
+  },
+  {
+    id: "3",
+    image: "https://example.com/images/leonardo-dicaprio.jpg",
+    name: "Leonardo DiCaprio",
+    profession: "Actor",
+  },
+  {
+    id: "4",
+    image: "https://example.com/images/taylor-swift.jpg",
+    name: "Taylor Swift",
+    profession: "Singer",
+  },
+  {
+    id: "5",
+    image: "https://example.com/images/elon-musk.jpg",
+    name: "Elon Musk",
+    profession: "Entrepreneur",
+  },
+  {
+    id: "6",
+    image: "https://example.com/images/serena-williams.jpg",
+    name: "Serena Williams",
+    profession: "Tennis Player",
+  },
+  {
+    id: "7",
+    image: "https://example.com/images/chris-hemsworth.jpg",
+    name: "Chris Hemsworth",
+    profession: "Actor",
+  },
+  {
+    id: "8",
+    image: "https://example.com/images/bill-gates.jpg",
+    name: "Bill Gates",
+    profession: "Entrepreneur",
+  },
+  {
+    id: "9",
+    image: "https://example.com/images/rihanna.jpg",
+    name: "Rihanna",
+    profession: "Singer",
+  },
+  {
+    id: "10",
+    image: "https://example.com/images/michael-jordan.jpg",
+    name: "Michael Jordan",
+    profession: "Basketball Player",
+  },
+];
