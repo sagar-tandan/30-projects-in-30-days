f6e94f9851eb10815e9bf35d92bce1981347341c
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-25 10:55:16 +0545

    js in middle

--- Day 9-14-Todo/scripts.js
+++ Day 9-14-Todo/scripts.js
@@ -1,11 +1,15 @@
 const todoedit = document.querySelector(".todo-list");
 const button = document.getElementById("btn");
-
-
-
-
+const container = document.querySelector(".container");
+// const allTodo = document.querySelector(".allTodo");
+const allTodo = document.createElement("div");
+allTodo.classList.add("allTodo");
 
 let finaltodo = [];
+
+getTodo();
+
+console.log(finaltodo);
 
 button.addEventListener("click", () => {
   const currentTodo = todoedit.value.trim();
@@ -16,7 +20,45 @@ button.addEventListener("click", () => {
   }
 });
 
+function getTodo() {
+  const allTodos = localStorage.getItem("TODO");
+  if (allTodos) {
+    container.appendChild(allTodo);
+    const todoArray = JSON.parse(allTodos);
+    todoArray.forEach((todo) => {
+      finaltodo.push({id: 1, value: todo.value, checked: todo.checked });
+    });
+  }
+}
+
 function addTodo(todo) {
   finaltodo.push({ value: todo, checked: false });
-  console.log(finaltodo);
+  // console.log(finaltodo);
+  localStorage.setItem("TODO", JSON.stringify(finaltodo));
+  todoedit.value = "";
+  location.reload();
 }
+
+function mapTodo() {
+  finaltodo.forEach((todo) => {
+    const oneTodo = document.createElement("div");
+    oneTodo.classList.add("oneTodo");
+    const input1 = document.createElement("input");
+    input1.id = "checkbox";
+    input1.type = "checkbox";
+    oneTodo.appendChild(input1);
+    const input2 = document.createElement("input");
+    input2.id = "actualtodo";
+    input2.type = "text";
+    input2.value = todo.value;
+    input2.disabled = true;
+    oneTodo.appendChild(input2);
+    const image = document.createElement("img");
+    image.src = "https://cdn-icons-png.flaticon.com/128/6861/6861362.png";
+    image.classList.add("delete");
+    oneTodo.appendChild(image);
+    allTodo.appendChild(oneTodo);
+  });
+}
+
+mapTodo();
