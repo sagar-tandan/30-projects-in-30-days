0f66928089f9ef4bbca4fde19658a96269a85903
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-26 15:34:37 +0545

    draft

--- Day 9-15- Netflix/scripts.js
+++ Day 9-15- Netflix/scripts.js
@@ -2,8 +2,16 @@ const menu = document.querySelector(".menu");
 const white = document.querySelector(".white");
 const red = document.querySelector(".red");
 const black = document.querySelector(".black");
+
+
 menu.addEventListener("click", () => {
   white.classList.add("active");
-  red.classList.add("active");
-  black.classList.add("active");
+
+  setTimeout(() => {
+    red.classList.add("active");
+  }, 200);
+
+  setTimeout(() => {
+    black.classList.add("active");
+  }, 400);
 });
