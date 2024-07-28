a2e34f563daecbb24d8bbd0c8bbd3c9a3bd59365
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-28 21:58:56 +0545

    js 1 done

--- Day 17- Photo Gallery/scripts.js
+++ Day 17- Photo Gallery/scripts.js
@@ -1,9 +1,13 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("button");
+const info = document.getElementById("value");
 
 btn.addEventListener("click", () => handleClick());
 
 function handleClick() {
-  console.log(input.value);
-
+  if (input.value < 1 || input.value > 10) {
+    info.classList.remove('hidden')
+  }else{
+    info.classList.add('hidden')
+  }
 }
