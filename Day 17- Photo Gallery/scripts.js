823bae2f9deb999db4b5dfdba1bcd5b89b865ba5
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-28 22:11:50 +0545

    photos fetched

--- Day 17- Photo Gallery/scripts.js
+++ Day 17- Photo Gallery/scripts.js
@@ -2,12 +2,29 @@ const input = document.getElementById("input");
 const btn = document.getElementById("button");
 const info = document.getElementById("value");
 
+let allPhotos = [];
+
 btn.addEventListener("click", () => handleClick());
 
 function handleClick() {
   if (input.value < 1 || input.value > 10) {
-    info.classList.remove('hidden')
-  }else{
-    info.classList.add('hidden')
+    info.classList.remove("hidden");
+  } else {
+    info.classList.add("hidden");
+    fetchPhotos(input.value);
   }
+}
+
+async function fetchPhotos(number) {
+  const response = await fetch(
+    `https://api.unsplash.com/photos?per_page=${number}&page=${Math.round(
+      Math.random() * 1000
+    )}&client_id=B8S3zB8gCPVCvzpAhCRdfXg_aki8PZM_q5pAyzDUvlc`
+  );
+  allPhotos = await response.json();
+  MapallPhotos();
+}
+
+function MapallPhotos (){
+    
 }
