e3432ec10d934dfadfa448990d4e4a001f407761
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-29 15:54:27 +0545

    Fisher Yates Shuffle algo added

--- Day 9-18- Memory Game/scripts.js
+++ Day 9-18- Memory Game/scripts.js
@@ -1,7 +1,45 @@
 const cards = document.querySelectorAll(".card");
 
-cards.forEach((card) => {
+const colors = [
+  "#3352d6",
+  "#3352d6",
+  "#fe0000",
+  "#fe0000",
+  "#20ab8e",
+  "#20ab8e",
+  "#8052ec",
+  "#8052ec",
+  "#121212",
+  "#121212",
+  "#f87316",
+  "#f87316",
+  "#fbc20c",
+  "#fbc20c",
+  "#d4b59e",
+  "#d4b59e",
+  "#414e55",
+  "#414e55",
+  "#16a34a",
+  "#16a34a",
+];
+
+//Fisher Yates Shuffling Algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Shuffle the colors
+const shuffledColors = shuffleArray([...colors]);
+
+cards.forEach((card, index) => {
   card.addEventListener("click", () => {
     card.classList.toggle("active");
+    if (card.className.includes("active")) {
+      card.style.backgroundColor = shuffledColors[index];
+    }
   });
 });
