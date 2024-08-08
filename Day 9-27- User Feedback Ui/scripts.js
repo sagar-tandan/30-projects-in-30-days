04749fe214dfeffcaf79cb449e5c05bece5531ff
Sagar Tandan <076bct072@ioepc.edu.np>
2024-08-08 13:17:36 +0545

    Day 27 Completed

--- Day 9-27- User Feedback Ui/scripts.js
+++ Day 9-27- User Feedback Ui/scripts.js
@@ -3,8 +3,10 @@ const message = document.querySelector(".message");
 const submit = document.querySelector(".submit");
 const cancel = document.querySelector(".cancel");
 const errorMessage = document.querySelector(".errorMessage");
+const container = document.querySelector(".container");
 
 let Content = [];
+let back;
 
 function removeActive() {
   emotions.forEach((emotion) => {
@@ -19,6 +21,7 @@ emotions.forEach((emotion) => {
 });
 
 submit.addEventListener("click", () => {
+  Content = [];
   if (message.value == "") {
     console.log(errorMessage);
     errorMessage.innerHTML = "Please mention the reason.";
@@ -26,20 +29,43 @@ submit.addEventListener("click", () => {
   } else {
     errorMessage.innerHTML = "";
     message.style.border = `2px solid lightblue`;
-    const emotion = findEmotions();
-    if (emotion == null) {
+    const em = findEmotions();
+    if (em == null) {
       errorMessage.innerHTML = "Please Choose the reaction emojies!";
     }
     const review = message.value;
-    Content.push({ emotion: emotion, message: review });
+    Content.push({ emotion: em, message: review });
     console.log(Content);
+    LoadThankYou();
   }
 });
 
 function findEmotions() {
+  let em;
   emotions.forEach((emotion) => {
     if (emotion.classList.contains("active")) {
-      return emotion.innerText;
+      em = emotion.querySelector("h3").innerText;
     }
+  });
+  return em;
+}
+
+function LoadThankYou() {
+  container.innerHTML = `<div class="feedback">
+        <img
+          class="thankUimg"
+          src="https://cdn-icons-png.flaticon.com/256/1208/1208754.png"
+          alt=""
+        />
+        <h2 class="thankUmessage">Thank you for your feedback</h2>
+        <p class="thankUpara">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+          exercitationem quo.
+        </p>
+        <button class="back">Back to Home</button>
+      </div>`;
+  back = document.querySelector(".back");
+  back.addEventListener("click", () => {
+    location.reload();
   });
 }
